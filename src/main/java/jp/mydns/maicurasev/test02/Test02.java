4b9069807f0742d46a2207a0f603aeea29063e6f
kuwatan <muratakuma@outlook.com>
2024-02-21 21:45:43 +0900

    Initial commit

---
+++ src/main/java/jp/mydns/maicurasev/test02/Test02.java
@@ -0,0 +1,30 @@
+package jp.mydns.maicurasev.test02;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class Test02 extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+        public void onChat(AsyncPlayerChatEvent event) {
+        // ifがgetMessageがメッセージ内容取得,equals("hello")が文字列のif
+        if(event.getMessage().equals("hello")){
+            // ifの内部処理で、helloだったときsetMessageで文字を変える
+            event.setMessage("こんにちは、"+event.getPlayer().getDisplayName());
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
