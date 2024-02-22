e02364b9a138cc95283b976561b2c5456b495508
kuwatan <muratakuma@outlook.com>
2024-02-23 07:46:37 +0900

    コード追加

--- src/main/java/jp/mydns/maicurasev/test02/Test02.java
+++ src/main/java/jp/mydns/maicurasev/test02/Test02.java
@@ -1,8 +1,10 @@
 package jp.mydns.maicurasev.test02;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -22,6 +24,10 @@ public final class Test02 extends JavaPlugin implements Listener {
             event.setMessage("こんにちは、"+event.getPlayer().getDisplayName());
         }
     }
+    public void join(PlayerJoinEvent event) {
+        // プレイヤー参加メッセージ変更
+         event.setJoinMessage(ChatColor.YELLOW+"サーバーに"+event.getPlayer().getDisplayName() + "が参加しました！");
+     }
 
     @Override
     public void onDisable() {
